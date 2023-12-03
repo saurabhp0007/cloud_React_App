0def5835249a0a37fc554116db6abe7df540fed8
saurabh kumar pandey <saurabhpanday21212@gmail.com>
2023-12-03 22:45:18 +0530

    code
    code

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import About from './Components/About'
+import AllInOne from './Components/Allinone'
+import Footer from './Components/Footer'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import Pricing from './Components/Pricing'
+import Support from './Components/Support'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
